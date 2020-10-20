1ffb32b9357724369cb0b73157fff88e8b820b6f
James Ikubi <james.ikubi@kenya.cellulant.com>
2020-10-20 12:13:13 +0300

    initial commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,39 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { Recipe } from './recipe.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  
+  private recipes: Recipe[] = [
+    new Recipe('A sweet recipe', 
+    'This is simply a test',
+     'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+     [
+      new Ingredient('Meat', 1),
+      new Ingredient('French Fries', 20)
+    ]),
+    new Recipe('Recipe tamu',
+      'This is simply another test',
+      'https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
+  ];
+  
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+  
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
